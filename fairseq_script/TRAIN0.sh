8d044827e2e8f41b8100529f192f056c648b80cd
Hieu Trung Tran <heraclex12@gmail.com>
2021-02-26 11:41:51 +0700

    Update TRAIN0.sh

--- fairseq_script/TRAIN0.sh
+++ fairseq_script/TRAIN0.sh
@@ -8,7 +8,7 @@ DIR=$(cd $(dirname $0); pwd)
 CODE=$DIR/nl2sparql
 export PYTHONPATH="$CODE:$PYTHONPATH"
 
-BERT_MODEL=bert-base-uncased
+BERT_MODEL=$DIR/uncased_L-12_H-768_A-12
 CORPUS=$DIR/corpus
 SRC=en
 TRG=sparql
@@ -20,6 +20,20 @@ mkdir -p $CORPUS
 cp ../../drive/MyDrive/THESIS/tntspa/data/LC-QUAD/train.* $CORPUS
 cp ../../drive/MyDrive/THESIS/tntspa/data/LC-QUAD/test.* $CORPUS
 cp ../../drive/MyDrive/THESIS/tntspa/data/LC-QUAD/dev.* $CORPUS
+
+#
+# Download the BERT model
+#
+if [ ! -d $BERT_MODEL ]; then
+    wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip
+    unzip uncased_L-12_H-768_A-12.zip
+    (cd $BERT_MODEL ; \
+     ln -s bert_config.json config.json ; \
+     transformers-cli convert --model_type bert \
+	--tf_checkpoint bert_model.ckpt \
+	--config bert_config.json \
+	--pytorch_dump_output pytorch_model.bin)
+fi
 
 #
 # Train sub-word models
